be583ed9f53dddc54f1fdf86cf9e1045fa006778
andres <ads.dark@gmail.com>
2024-04-25 00:20:45 -0400

    manual de limpieza para dormitorio

---
+++ retos_sesion_03/ejercicio_03.py
@@ -0,0 +1,21 @@
+print(".......Como limpiar una habitacion segun recomendaciones de personas............")
+print("1. Ventilar mínimo 15 minutos")
+print("En primer lugar, Paula Seiton recomienda airear toda la casa antes de comenzar a limpiar el dormitorio por varias razones, la más importante es porque la ventilación permite que el aire fresco circule y elimine olores acumulados, lo que hace que el ambiente sea más agradable y saludable. Además, la ventilación ayuda a eliminar la concentración de contaminantes en el aire, como polvo, humo y alérgenos. Además, al reducir la cantidad de polvo y ácaros, la limpieza posterior te será mucho más fácil.")
+
+print("2. Recoger y ordenar la estancia")
+print("Antes de empezar con la limpieza, la experta recomienda recoger todos los trastos desperdigados por el dormitorio y ponerlos en su sitio. Si hay objetos o prendas tiradas por el suelo o encima de la cama/muebles, te será mucho más difícil limpiar, tardarás mucho más y encima no limpiarás las superficies como es debido, por lo que en unos días volverás a notar que está todo sucio.  Por otro lado, ordenar cualquier estancia, incluida el dormitorio, ayuda a reducir el desorden y caos en el ambiente, cosa que acabará afectando positivamente en tu estado de ánimo y en la sensación de bienestar.")
+
+print("3. Hacer la cama tras ventilar")
+print("Tal y como indica Paula, hacer la cama es una muy buena práctica en general, pero si la haces después de ventilar, ¡aún mejor! Así el aire habrá ayudado a que se despejen los ácaros y polvo de entre la ropa de cama y esta quedará mucho más limpia y libre de alérgenos. Además, una cama bien hecha transmite calma y armonía, lo que ayudará a que la habitación parezca automáticamente mucho más limpia y ordenada.")
+
+print("4. Aspirar, incidiendo en las alfombras")
+print("El aspirado es una forma efectiva de eliminar el polvo y la suciedad que suelen acumularse en el suelo, bajo los muebles y en las alfombras, ya que si no se limpian regularmente pueden convertirse en un caldo de cultivo de ácaros y otros alérgenos. Eso sí; si tienen manchas, trátalas antes. Lo mismo para los muebles tipo sillones y sillas que puedas tener dentro de tu habitación:cuida su tapicería aspirando por toda la superficie empezando por las esquinas. Así atraparás la mayor cantidad de pelos y suciedad posible.")
+
+print("5. Limpiar de arriba abajo")
+print("Para la experta en limpieza, la mejor y única forma correcta de limpiar y quitar el polvo es comenzando por los muebles superiores y acabando en los inferiores. Es una estrategia supereficaz, y siguiéndola al pie de la letra evitarás que la suciedad y el polvo caigan sobre los muebles que ya has limpiado. Así no harás doble trabajo inútilmente y luego te será mucho más fácil retirar de sola una pasada el polvo que haya ido cayendo en los muebles más bajos.")
+
+print("6. Eliminar el polvo de las paredes")
+print("Muchas personas no piensan en esto, pero es tan importante limpiar los muebles como mantener las paredes de una habitación limpias. Así, empieza por atacar primero las manchas que haya en las paredes. Es recomendable limpiarlas primero con agua jabonosa antes de proceder a quitar el polvo. Luego, se puede utilizar una escoba suave, un plumero o un paño de microfibra para quitar el polvo de la superficie de la pared, trabajando de arriba hacia abajo para cubrir toda la superficie.")
+
+print("9. Terminar limpiando el suelo")
+print("Fregar el suelo siempre debe estar en ultimísimo lugar. Es importante recordar que, independientemente del tipo de suelo, debes evitar utilizar productos abrasivos o limpiadores fuertes que puedan dañarlo. Así, Seiton recomienda aspirar o barrer siempre con cuidado y luego preparar una solución de vinagre blanco y agua tibia si se trata de un suelo de madera o parqué. Ayúdate de una mopa o fregona suave de microfibra para no dañar el suelo. Si deseas un aroma agradable, puedes agregar unas gotas de aceite esencial a la mezcla.")
