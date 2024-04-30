3b9a1335672b5d8684fbc3cb4eaa222e0b1ae426
andres <ads.dark@gmail.com>
2024-04-30 00:46:31 -0400

    3 instrucciones buscaminas

---
+++ retos_sesion_04/ejercicio_03.py
@@ -0,0 +1,27 @@
+print("""
+    no conozco muchos juegos de mesa
+    si los conosco, no me gustan.
+    Solo me gusta de forma digital.
+
+    *********BUSCA MINAS**********
+    """)
+print("""
+    ------------REGLAS------------
+    *El juego consiste en despejar todas 
+    las casillas de una pantalla que no oculten una mina.
+    
+    *Algunas casillas tienen un número, este número indica las minas 
+    que son en todas las casillas circundantes. Así, si una casilla tiene el número 3, 
+    significa que de las ocho casillas que hay alrededor (si no es en una esquina o borde) 
+    hay 3 con minas y 5 sin minas.
+    
+    *Si se descubre una casilla sin número indica que ninguna 
+    de las casillas vecinas tiene mina y estas se descubren automáticamente.
+    
+    *Si se descubre una casilla con una mina se pierde la partida.
+    
+    *Se puede poner una marca en las casillas que el jugador piensa 
+    que hay minas para ayudar a descubrir la que están cerca.
+
+    """)
+
