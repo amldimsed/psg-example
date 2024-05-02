de712ae5045ad11911cbf74205c27ce465aaf048
andres <ads.dark@gmail.com>
2024-05-02 01:42:52 -0400

    ejercicio 2

---
+++ retos_sesion_05/ejercicio_02.py
@@ -0,0 +1,4 @@
+# (n °C × 9/5) + 32 = miau °F
+print("30°C conversion a °F ==> " , ((30 * 9/5) + 32) , "°F")
+print("-273.99°C conversion a °F ==>  %.3f" %((-273.99 * 9/5) + 32) , "°F")
+print("100°C conversion a °F ==> " , ((100 * 9/5) + 32) , "°F")
