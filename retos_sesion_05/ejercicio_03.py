c5039112b5ac9c9135c13bd3b012e993723bdff0
andres <ads.dark@gmail.com>
2024-05-02 01:43:03 -0400

    ejercicio 3

---
+++ retos_sesion_05/ejercicio_03.py
@@ -0,0 +1,9 @@
+# segundos: 288325
+# costumbre de java, c++, c#... 
+dia:int; hora:int; minuto:int
+rest_hora:int; rest_min:int; rest_seg:int
+
+minuto = 288325 // 60; rest_seg = 288325 % 60
+hora = minuto // 60; rest_min = minuto % 60
+dia = hora // 24; rest_hora = hora % 24 
+print("son ", dia, "dias", rest_hora, "horas", rest_min, "minutos", rest_seg, "segundos")
